507f75dda3c206d1b1877fcefb5f52bfbfa08acd
gilby125 <gilby1@gmail.com>
2025-06-05 09:54:28 -0500

    .

--- init.sql
+++ init.sql
@@ -1,7 +1,2 @@
--- Initialize flight analysis database
-CREATE USER flight_user WITH PASSWORD 'flight_pass';
+-- Minimal initialization script
 CREATE DATABASE flight_db;
-GRANT ALL PRIVILEGES ON DATABASE flight_db TO flight_user;
-
--- Create extension if needed
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
